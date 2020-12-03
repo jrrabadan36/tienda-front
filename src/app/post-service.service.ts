488a81775e0ff84c6d7a5bf1ed87fa4689374189
Jose ramón <jrra35@gmail.com>
2020-12-03 13:12:09 +0100

    se ha cambiaro el origen de la url a enviroment

--- src/app/post-service.service.ts
+++ src/app/post-service.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostServiceService {
 
-  url: string = 'https://jsonplaceholder.typicode.com/posts';
+  //url: string = 'https://jsonplaceholder.typicode.com/posts';
   headers = new HttpHeaders().set('Content-Type', 'application/json');
 
   constructor(private http: HttpClient) { }
 
   getPost() : Observable<any> {
-    return this.http.get(`${this.url}`);
+    return this.http.get(environment.url); // `${this.url}`
   }
 }
